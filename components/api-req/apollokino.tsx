73665feeaec3cf991e1a8e5a6b2ff8b6009750df
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-11-05 22:08:17 +0200

    added apis for cinemas

---
+++ components/api-req/apollokino.tsx
@@ -0,0 +1,100 @@
+interface Show {
+  ID: number;
+  AvailableSeats: number | null;
+  TotalSeats: number | null;
+  dtAccounting: string;
+  dttmShowStart: string;
+  dttmShowStartUTC: string;
+  dttmShowEnd: string;
+  dttmShowEndUTC: string;
+  ShowSalesStartTime: string;
+  ShowSalesStartTimeUTC: string;
+  ShowSalesEndTime: string;
+  ShowSalesEndTimeUTC: string;
+  ShowReservationStartTime: string;
+  ShowReservationStartTimeUTC: string;
+  ShowReservationEndTime: string;
+  ShowReservationEndTimeUTC: string;
+  EventID: number;
+  Title: string;
+  OriginalTitle: string;
+  ProductionYear: number;
+  LengthInMinutes: number;
+  dtLocalRelease: string;
+  Rating: string;
+  RatingLabel: string;
+  RatingImageUrl: string;
+  EventType: string;
+  Genres: string;
+  TheatreID: number;
+  TheatreAuditriumID: number;
+  Theatre: string;
+  TheatreAuditorium: string;
+  TheatreAndAuditorium: string;
+  PresentationMethod: string;
+  EventSeries: string;
+  ShowURL: string;
+  EventURL: string;
+  SpokenLanguage: {
+    Name: string;
+    NameInLanguage: string;
+    ISOTwoLetterCode: string;
+  };
+  Images: {
+    EventMediumImagePortrait: string | null;
+  };
+  ContentDescriptors: any[];
+}
+
+interface ScheduleData {
+  PubDate: string;
+  Shows: Show[];
+}
+
+export default async function ApolloKino() {
+  const url = "https://www.apollokino.ee/xml/Schedule";
+
+  try {
+    const response = await fetch(url, { cache: "no-store" });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+
+    const jsonData: ScheduleData = await response.json();
+    console.log(jsonData);
+
+    return (
+      <div>
+        <h1>Schedule</h1>
+        {jsonData.Shows.map((show, index) => (
+          <div key={index}>
+            <h2>{show.Title}</h2>
+            <p>
+              <strong>Original Title:</strong> {show.OriginalTitle}
+            </p>
+            <p>
+              <strong>Show Time:</strong> {show.dttmShowStart}
+            </p>
+            <p>
+              <strong>Location:</strong> {show.TheatreAndAuditorium}
+            </p>
+            <p>
+              <strong>Genres:</strong> {show.Genres}
+            </p>
+            {show.Images.EventMediumImagePortrait && (
+              <img
+                src={show.Images.EventMediumImagePortrait}
+                alt={show.Title}
+                width="100"
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching schedule data:", error);
+    return <p>Error loading schedule data</p>;
+  }
+}
---
+++ components/api-req/forum.tsx
@@ -0,0 +1,118 @@
+import { parseStringPromise } from "xml2js";
+
+interface Show {
+  ID: number;
+  AvailableSeats: number | null;
+  TotalSeats: number | null;
+  dtAccounting: string;
+  dttmShowStart: string;
+  dttmShowStartUTC: string;
+  dttmShowEnd: string;
+  dttmShowEndUTC: string;
+  ShowSalesStartTime: string;
+  ShowSalesStartTimeUTC: string;
+  ShowSalesEndTime: string;
+  ShowSalesEndTimeUTC: string;
+  ShowReservationStartTime: string;
+  ShowReservationStartTimeUTC: string;
+  ShowReservationEndTime: string;
+  ShowReservationEndTimeUTC: string;
+  EventID: number;
+  Title: string;
+  OriginalTitle: string;
+  ProductionYear: number;
+  LengthInMinutes: number;
+  dtLocalRelease: string;
+  Rating: string;
+  RatingLabel: string;
+  RatingImageUrl: string;
+  EventType: string;
+  Genres: string;
+  TheatreID: number;
+  TheatreAuditriumID: number;
+  Theatre: string;
+  TheatreAuditorium: string;
+  TheatreAndAuditorium: string;
+  PresentationMethod: string;
+  EventSeries: string;
+  ShowURL: string;
+  EventURL: string;
+  SpokenLanguage: {
+    Name: string;
+    NameInLanguage: string;
+    ISOTwoLetterCode: string;
+  };
+  Images: {
+    EventMediumImagePortrait: string | null;
+  };
+  ContentDescriptors: any[];
+}
+
+interface Show {
+  Show: Show[];
+}
+
+interface ScheduleData {
+  PubDate: string;
+  Shows: Show;
+}
+
+interface FullData {
+  Schedule: ScheduleData;
+}
+
+export default async function Forum() {
+  const url = "https://www.forumcinemas.ee/xml/Schedule";
+
+  try {
+    const response = await fetch(url, { cache: "no-store" });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+
+    //const jsonData: ScheduleData = await response.json();
+
+    const xmlData: any = await response.text();
+    //console.log(xmlData);
+    const jsonData: FullData = await parseStringPromise(xmlData);
+    /*console.log(jsonData);
+    console.log(jsonData.Schedule);
+    console.log(jsonData.Schedule.Shows);
+    console.log(jsonData.Schedule.Shows[0]);
+    console.log(jsonData.Schedule.Shows[0].Show);*/
+
+    return (
+      <div>
+        <h1>Schedule</h1>
+        {jsonData.Schedule.Shows[0].Show.map((show, index) => (
+          <div key={index}>
+            <h2>{show.Title}</h2>
+            <p>
+              <strong>Original Title:</strong> {show.OriginalTitle}
+            </p>
+            <p>
+              <strong>Show Time:</strong> {show.dttmShowStart}
+            </p>
+            <p>
+              <strong>Location:</strong> {show.TheatreAndAuditorium}
+            </p>
+            <p>
+              <strong>Genres:</strong> {show.Genres}
+            </p>
+            {show.Images.EventMediumImagePortrait && (
+              <img
+                src={show.Images.EventMediumImagePortrait}
+                alt={show.Title}
+                width="100"
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching schedule data:", error);
+    return <p>Error loading schedule data</p>;
+  }
+}
