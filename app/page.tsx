46ebd6d3379fff87fe420bc38464c81371a80192
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-11-05 22:08:17 +0200

    added apis for cinemas

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import ApolloKino from "@/components/api-req/apollokino";
+import Artis from "@/components/api-req/artis";
+import Forum from "@/components/api-req/forum";
+import Thule from "@/components/api-req/thule";
+import Viimsi from "@/components/api-req/viimsi";
 
 export default async function Index() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <h1 className="bold text-pink-500">Apollokino</h1>
+        <ApolloKino />
+        <hr />
+        <h1 className="bold text-pink-500">Artis</h1>
+        <Artis />
+        <hr />
+        <h1 className="bold text-pink-500">Viimsi</h1>
+        <Viimsi />
+        <hr />
+        <h1 className="bold text-pink-500">Thule</h1>
+        <Thule />
+        <hr />
+        <h1 className="bold text-pink-500">Forum</h1>
+        <Forum />
       </main>
     </>
   );
