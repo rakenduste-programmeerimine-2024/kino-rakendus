15259d9adc696a1190628e0c032bb7a5bd039489
Jan-Erich <146342622+JanErichS@users.noreply.github.com>
2024-11-14 17:53:18 +0200

    refactor: replace prop drilling with context and simplify component structure in Signup

--- components/signup/membership.types.ts
+++ components/signup/membership.types.ts
@@ -1,23 +1,10 @@
 export interface ICinemaState {
-  showForum: boolean;
-  showApollo: boolean;
-  showArtis: boolean;
-  showThule: boolean;
-  showViimsi: boolean;
-}
-
-export interface IMembership {
-  selectedMemberships: string[];
-  setSelectedMemberships: React.Dispatch<React.SetStateAction<string[]>>;
+  id: number;
+  name: string;
+  isChecked: boolean;
 }
 
 export interface IMembershipTier {
   id: number;
   title: string;
-}
-
-export interface IMembershipTierTypes {
-  selectedMembershipTier: IMembershipTier[];
-  setSelectedMembershipTier: React.Dispatch<React.SetStateAction<IMembershipTier[]>>;
-  allTiers?: IMembershipTier[]; // Stores the fetched tiers
 }
