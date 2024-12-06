f94f56d75f9f160e64f7041bfff8cbdcc28337a5
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-12-06 17:58:46 +0200

    fix: working

--- lib/price/apollo-price.ts
+++ lib/price/apollo-price.ts
@@ -1,10 +1,13 @@
-import { createClient } from "@/utils/supabase/server";
+"use client"
+
+import { createClient } from "@/utils/supabase/client";
 import { Show } from "../movie-data/cinemas/apollo-types";
 
 
 
 export default function apolloPriceCalculation(show: Show):string{
     const supabase = createClient();
+    console.log(supabase.storage.listBuckets())
     let price: string = "tekst";
     return price
 }
