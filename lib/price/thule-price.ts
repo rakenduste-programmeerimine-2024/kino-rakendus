5cfec1024c4f1731a5d8a4d7c1a3c76f005da87a
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-12-07 16:34:05 +0200

    feat: added thule prices

--- lib/price/thule-price.ts
+++ lib/price/thule-price.ts
@@ -2,16 +2,16 @@ import { Show } from "../movie-data/cinemas/thule-types";
 
 export default function thulePriceCalculation(show: Show):number{
     //const [date, time] = show.dttmShowStart.split('T')
-    let price: number = "Hinda ei leitud";
+    let price: number = 0;
     const dateTime = new Date(show.dttmShowStart);
-    if(dateTime.getDay() == 4){
-        price = "4,50€"
-    } else if(dateTime.getDay() <= 4 && dateTime.getHours() <= 17){
-        price = "5.50€"
-    } else if(dateTime.getDay() <= 3 && dateTime.getHours() > 17){
-        price = "8,20€"
+    if(dateTime.getDay() == 0 || dateTime.getDay() == 6){
+        price = 7
+    } else if(dateTime.getDay() == 4){
+        price = 4,50
+    } else if(dateTime.getDay() <= 3 && dateTime.getHours() <= 17){
+        price = 5.50
     } else {
-        price = "9,90€"
+        price = 7
     }
     return price
 }
