d90b65cc2d2e836429a7cc8e8022b4e1cc044bf5
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-12-07 22:00:06 +0200

    fix: artis price fix

--- lib/price/artis-price.ts
+++ lib/price/artis-price.ts
@@ -8,13 +8,13 @@ export default function artisPriceCalculation(show: Show):string{
     let price: number = -1;
     const dateTime = new Date(show.dttmShowStart);
     if(dateTime.getDay() == 0){
-        price = 9.90
+        price = 8.90
     } else if(dateTime.getDay() <= 5 && dateTime.getHours() <= 17){
         price = 7
     } else if(dateTime.getDay() <= 4 && dateTime.getHours() > 17){
         price = 8.20
     } else {
-        price = 9.90
+        price = 8.90
     }
     if(price == -1){
         return "Hinda ei leitud"
