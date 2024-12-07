a88702885c7c3db837f163a00b146d8026c0ae29
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-12-07 16:50:38 +0200

    fix: prices now have correct number format

--- lib/price/thule-price.ts
+++ lib/price/thule-price.ts
@@ -7,7 +7,7 @@ export default function thulePriceCalculation(show: Show):number{
     if(dateTime.getDay() == 0 || dateTime.getDay() == 6){
         price = 7
     } else if(dateTime.getDay() == 4){
-        price = 4,50
+        price = 4.50
     } else if(dateTime.getDay() <= 3 && dateTime.getHours() <= 17){
         price = 5.50
     } else {
